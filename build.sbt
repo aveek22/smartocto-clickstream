b6d2b0dc6ca370243c907e9da2727569371657cf
Aveek Das <aveek.das@dpgmedia.be>
2022-07-28 14:48:41 +0200

    BasicStreaming

---
+++ build.sbt
@@ -0,0 +1,36 @@
+name := "smartocto-learning"
+
+version := "0.1-SNAPSHOT"
+
+organization := "net.persgroep"
+
+ThisBuild / scalaVersion := "2.12.16"
+
+val flinkVersion = "1.13.6"
+
+val flinkDependencies = Seq(
+  "org.apache.flink" %% "flink-clients" % flinkVersion,
+  "org.apache.flink" %% "flink-scala" % flinkVersion,
+  "org.apache.flink" %% "flink-streaming-scala" % flinkVersion,
+  "org.apache.flink" %% "flink-connector-kafka" % flinkVersion)
+
+lazy val root = (project in file(".")).
+  settings(
+    libraryDependencies ++= flinkDependencies,
+    assembly / mainClass := Some("net.persgroep.basic.BasicStreaming"),
+  )
+
+// assembly / mainClass := Some("net.persgroep.basic.BasicStreaming")
+
+// make run command include the provided dependencies
+// Compile / run  := Defaults.runTask(Compile / fullClasspath,
+//                                    Compile / run / mainRunner,
+//                                    Compile / run / runner
+//                                   ).evaluated
+
+// // stays inside the sbt console when we press "ctrl-c" while a Flink programme executes with "run" or "runMain"
+// Compile / run / fork := true
+// Global / cancelable := true
+
+// exclude Scala library from assembly
+// assembly / assemblyOption  := (assembly / assemblyOption).value.copy(includeScala = false)
---
+++ src/main/scala/net/persgroep/basic/BasicStreaming.scala
@@ -0,0 +1,60 @@
+package net.persgroep.basic
+
+/*
+* This is a basic streaming program that reads string values from a kafka topic
+* and writes to another kafka topic.
+*
+* To compile simply run: sbt clean compile
+* To run locally: sbt run
+* 
+* The program will start execution locally, need to open the kafka console producer and consumer on 
+* two different terminals to view the outputs.
+* 
+*/
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+
+import java.util.Properties
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer
+
+object BasicStreaming {
+
+  def main(args: Array[String]): Unit = {
+    
+    // Create the flink execution environment
+    val flinkEnv: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+
+    // Setup Kafka consumer properties
+    val kafkaConsumerProperties = new Properties()
+    kafkaConsumerProperties.setProperty("bootstrap.servers", "localhost:9092")
+    kafkaConsumerProperties.setProperty("group.id", "local")
+
+    val sourceTopic = "input-kafka-topic"
+    val deserializationSchema = new SimpleStringSchema()
+    val kafkaConsumer = new FlinkKafkaConsumer[String](sourceTopic, deserializationSchema, kafkaConsumerProperties)
+
+
+    // Setup Kafka producer properties
+    val kafkaProducerProperties = new Properties()
+    kafkaProducerProperties.setProperty("bootstrap.servers", "localhost:9092")
+
+    val destinationTopic = "output-kafka-topic"
+    val serializationSchema = new SimpleStringSchema()
+    val kafkaProducer = new FlinkKafkaProducer[String](destinationTopic, serializationSchema, kafkaProducerProperties)
+
+
+    // Prepare the event stream
+
+    val eventStream : DataStream[String] = flinkEnv.addSource(kafkaConsumer)
+    eventStream.addSink(kafkaProducer)
+
+
+    // Execute the flink program.
+    flinkEnv.execute("Local Flink Job")
+
+  }
+
+}
